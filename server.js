e98eedcc4ed9b2c355b845e0a3e9d64d696c11a6
Sasuke Uchiha <97214534+Sasuke031026@users.noreply.github.com>
2022-06-04 03:32:23 +1000

    update

--- server.js
+++ server.js
@@ -67,8 +67,7 @@ setInterval(() => {
             }
             break;
         case PLAYING:
-            console.log(Date.now() - startTime);
-            mul += 0.01 + (Date.now() - startTime) ^ 2 / 1000;
+            mul += 0.01 + (Date.now() - startTime) / 50000;
 
             if (mul > gameTime) {
                 GameState = GAMEEND;
